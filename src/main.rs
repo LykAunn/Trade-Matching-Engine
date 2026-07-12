6ec2e1efc7979155abe0320863137ef160df448c
LykAunn <lykaunn@gmail.com>
2026-07-12 17:11:00 +0800

    Get resting orders and order cancellation

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@ use orderbook::OrderBook;
 fn main() {
     let mut book = OrderBook::new();
 
-    book.submit(Order {id: 1, side: Side::Sell, price: 100,  quantity: 10});
-    let fills = book.submit(Order {id: 2, side: Side::Buy, price: 100, quantity: 6});
-
-    println!("{:?}", fills)
+    book.submit(Order {id: 134, side: Side::Buy, price: 100,  quantity: 10});
+    book.submit(Order {id:123, side: Side::Buy,price:101, quantity: 999});
+    let fills = book.submit(Order {id: 223, side: Side::Sell, price: 99, quantity: 6});
+    
+    println!("{:?}", fills);
+    println!("{:?}", book.get_outstanding(Side::Buy));
+    println!("{}", book.cancel_trade(134));
+    println!("{:?}", book.get_outstanding(Side::Buy));
 }
--- src/orderbook.rs
+++ src/orderbook.rs
@@ -1,14 +1,15 @@
 use crate::order::{Order, Side};
-use std::collections::BTreeMap;
+use std::{cmp::min, collections::{BTreeMap, HashMap}};
 
 pub struct OrderBook {
-    bids: BTreeMap<u64, Vec<Order>>,
-    asks: BTreeMap<u64, Vec<Order>>,
+    bids: BTreeMap<u64, Vec<Order>>, // Buy
+    asks: BTreeMap<u64, Vec<Order>>, // Sell
+    order_index: HashMap<u64, (Side, u64)> // order_id -> (side, price)
 }
 
 impl OrderBook {
     pub fn new() -> Self {
-        OrderBook {bids: BTreeMap::new(), asks: BTreeMap::new()}
+        OrderBook {bids: BTreeMap::new(), asks: BTreeMap::new(), order_index: HashMap::new()}
     }
 
     pub fn submit(&mut self, mut order: Order) -> Vec<(u64, u64, u64)> {
@@ -19,21 +20,31 @@ impl OrderBook {
         };
 
         let matching_prices: Vec<u64> = match order.side {
-            Side::Buy => book.range(..=order.price).map(|(p, _)| *p).collect(),
-            Side::Sell => book.range(..=order.price).map(|(p, _)| * p).collect(),
+            Side::Buy => book.range(..=order.price).map(|(p, _)| * p).collect(),
+            Side::Sell => book.range(order.price..).rev().map(|(p, _)| * p).collect(),
         };
 
-        for price in matching_prices{
+        for price in matching_prices {
             if order.quantity == 0 {break}
             if let Some(resting) = book.get_mut(&price) {
-                resting.retain_mut(|resting_order| {
-                    if order.quantity == 0 { return true; }
-                    let fill_qty = order.quantity.min(resting_order.quantity);
+                let mut i = 0;
+                while i < resting.len() {
+                    if order.quantity == 0 {break} // If there is no more to be filled, end
+
+                    let resting_order = &mut resting[i]; // Iterates through all resting orders of that price
+                    let fill_qty = min(order.quantity, resting_order.quantity);
                     fills.push((order.id, resting_order.id, fill_qty));
                     order.quantity -= fill_qty;
                     resting_order.quantity -= fill_qty;
-                    resting_order.quantity > 0
-                });
+
+                    if resting_order.quantity == 0 {
+                        let id: u64 = resting_order.id;
+                        resting.remove(i);
+                        self.order_index.remove(&id);
+                    } else {
+                        i += 1;
+                    }
+                }
             }
         }
 
@@ -42,9 +53,47 @@ impl OrderBook {
                 Side::Buy => &mut self.bids,
                 Side::Sell => &mut self.asks,
             };
+
+
+            self.order_index.insert(order.id, (order.side, order.price));
             resting_book.entry(order.price).or_insert_with(Vec::new).push(order);
         }
 
         fills
+    }
+
+    pub fn get_outstanding(&self, side: Side) -> Vec<Order> {
+        let book = match side {
+            Side::Buy => &self.bids,
+            Side::Sell => &self.asks
+        };
+
+        book.values().flatten().cloned().collect()
+    }
+
+    pub fn cancel_trade(&mut self, id: u64) -> bool {
+        if let Some(value) = self.order_index.get(&id) {
+            let order_side = value.0;
+            let order_price = value.1;
+
+            let book = match order_side {
+                Side::Buy => &mut self.bids,
+                Side:: Sell => &mut self.asks,
+            };
+
+            if let Some(matching) = book.get_mut(&order_price) {
+                for i in 0..matching.len() {
+                    if matching[i].id == id {
+                        matching.remove(i);
+                        self.order_index.remove(&id);
+                        return true;
+                    }
+                }
+            }
+            false
+        } else {
+            // not found in order_index
+            false
+        }
     }
 }
