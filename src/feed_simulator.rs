5dbd6da7fbd04c2e41e13203f8a7de626a5115ea
LykAunn <lykaunn@gmail.com>
2026-07-25 21:01:25 +0800

    Random order generation
    Created a new struct that generates random orders

---
+++ src/feed_simulator.rs
@@ -0,0 +1,69 @@
+use rand::{RngExt, rngs::ThreadRng};
+use rand_distr::{Exp, Distribution, Normal};
+use crate::order::{Order, Side, TradeType};
+
+pub struct FeedSimulator {
+    mid_price: f64,       // Current simulated mid price
+    rng: ThreadRng,       
+    next_id: u64,         
+    rate_per_second: f64, // orders per second
+    volatility: f64,      // size of random walk step
+    sim_time: f64,        // total simulated time
+    exp: Exp<f64>,
+    normal: Normal<f64>
+}
+
+impl FeedSimulator {
+    pub fn new(rate_per_second: f64, volatility: f64) -> Self {
+        FeedSimulator {
+            mid_price: 100.0,
+            rng: rand::rng(),
+            next_id: 1,
+            rate_per_second,
+            volatility,
+            sim_time: 0.0,
+            exp: Exp::new(rate_per_second).unwrap(),
+            normal : Normal::new(0.0, volatility).unwrap(),
+        }
+    }
+
+    pub fn generate_next(&mut self) -> (Order, f64) { // Return amount of time to sleep
+
+        // Time
+        let gap = self.exp.sample(&mut self.rng);
+        self.sim_time += gap;
+
+        // Price
+        let delta = self.normal.sample(&mut self.rng);
+        self.mid_price += delta;
+
+        // Side
+        let side = if self.rng.random_bool(0.5) {Side::Buy} else {Side::Sell};
+
+        // Buy/sell price
+        let price = match side{
+            Side::Buy => (self.mid_price - 2.0).round() as u64,
+            Side::Sell => (self.mid_price + 2.0).round() as u64
+        };
+
+        let quantity = self.rng.random_range(1..=100);
+
+        let id = self.next_id;
+        self.next_id += 1;
+
+        let order_type = match self.rng.random_range(1..100) {
+            0..=79 => TradeType::Limit,
+            80..=94 => TradeType::Market,
+            _ => TradeType::Ioc,
+        };
+
+        (Order {
+            id,
+            side,
+            price,
+            quantity,
+            trade_type: order_type
+        },
+        gap)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 mod order;
 mod orderbook;
+mod feed_simulator;
 
-use order::{Order, Side};
+use std::println;
+
+use order::{Side};
 use orderbook::OrderBook;
+use feed_simulator::FeedSimulator;
+
+use crate::order::Event;
 
 fn main() {
     let mut book = OrderBook::new();
-    let mut events = Vec::new();
+    let mut events: Vec<Event> = Vec::new();
+    let mut simulator = FeedSimulator::new(1.0, 1.0);
 
-    events.extend(book.submit(Order {id: 134, side: Side::Buy, price: 100,  quantity: 10, trade_type: order::TradeType::Limit}));
-    events.extend(book.submit(Order {id:123, side: Side::Sell, price:0, quantity: 999, trade_type: order::TradeType::Market}));
-    events.extend(book.submit(Order {id: 223, side: Side::Sell, price: 99, quantity: 6, trade_type: order::TradeType::Limit}));
-    
-    // println!("{:?}", event);
-    for event in events {
-        println!("{}", event.to_log_line());
+    for _ in 0..10 {
+        let order = simulator.generate_next();
+        let outcome = book.submit(order.0);
+        for event in &outcome {
+            println!("{}", event.to_log_line());
+        }
+        events.extend(outcome);
+        
     }
-    println!("{:?}", book.get_outstanding(Side::Buy));
+
+    // events.extend(book.submit(Order {id: 134, side: Side::Buy, price: 100,  quantity: 10, trade_type: order::TradeType::Limit}));
+    // events.extend(book.submit(Order {id:123, side: Side::Sell, price:0, quantity: 999, trade_type: order::TradeType::Market}));
+    // events.extend(book.submit(Order {id: 223, side: Side::Sell, price: 99, quantity: 6, trade_type: order::TradeType::Limit}));
+    
+    // for event in events {
+    //     println!("{}", event.to_log_line());
+    // }
     println!("{:?}", book.cancel_trade(134));
     println!("{:?}", book.get_outstanding(Side::Buy));
+    println!("{:?}", book.get_outstanding(Side::Sell));
 }
