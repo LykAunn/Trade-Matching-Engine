ddc90e8798a8da994115f26ca4a02c720222ca71
LykAunn <lykaunn@gmail.com>
2026-09-20 09:41:07 +0100

    Small refactorings and crossterm mouse inputs
    - Added cross-term mouse events
    - Removed unstable let-chain and replaced it with tuple matching
    - Refactored duplicate code

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,9 @@ use ratatui::{
 use ratatui::style::Color;
 use ratatui::widgets::canvas::{Canvas, Line, Rectangle};
 use std::{io, sync::mpsc, time::Duration, };
+use std::collections::BTreeMap;
 use std::thread;
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture, MouseButton, MouseEventKind};
 use ratatui::widgets::ListItem;
 use order::Side;
 
@@ -29,7 +31,7 @@ fn main() -> io::Result<()>{
     // --- terminal setup ---
     enable_raw_mode()?;
     let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen)?;
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout))?;
     
     let (order_tx, order_rx) = mpsc::channel::<Order>();
@@ -40,7 +42,7 @@ fn main() -> io::Result<()>{
     app.run(&mut terminal)?;
 
     disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture)?;
 
     Ok(())
 }
@@ -85,6 +87,7 @@ impl App {
     fn run(&mut self, terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> io::Result<()> {
         while !self.exit {
             while let Ok(order) = self.order_rx.try_recv() {
+                // If receive order from simulator, submit to order book
                 let updated_order = self.assign_id_to_order(order);
                 let outcome = self.book.submit(updated_order);
                 for event in &outcome {
@@ -95,18 +98,26 @@ impl App {
 
             terminal.draw(|frame| self.draw(frame))?;
 
+            // Cross term events
             if event::poll(Duration::from_millis(50))? {
-                if let CEvent::Key(key) = event::read()? {
-                    if key.code == KeyCode::Char('q') {
-                        self.exit = true;
-
-                    } else if key.code == KeyCode::Char('b') {
-                        self.submit_user_trade(Side::Buy);
-
-                    } else if key.code == KeyCode::Char('s') {
-                        self.submit_user_trade(Side::Sell);
-
+                match event::read()? {
+                    CEvent::Key(key) => {
+                        // Key event
+                        if key.code == KeyCode::Char('q') {
+                            self.exit = true;
+                        } else if key.code == KeyCode::Char('b') {
+                            self.submit_user_trade(Side::Buy);
+                        } else if key.code == KeyCode::Char('s') {
+                            self.submit_user_trade(Side::Sell);
+                        }
                     }
+                    CEvent::Mouse(mouse_event) => {
+                        // click handling
+                        if mouse_event.kind == MouseEventKind::Down(MouseButton::Middle) {
+                            self.exit = true;
+                        }
+                    }
+                    _ => {} // Ignore other events
                 }
             }
         }
@@ -134,9 +145,8 @@ impl App {
             .map(|(price, orders)| (*price, orders.iter()
                 .map(|o| o.quantity).sum::<u64>()));
 
-        if let Some((best_bid, bid_quantity)) = best_bid_info
-            && let Some((best_ask, ask_quantity)) = best_ask_info {
-                // Currently calculating in u64
+        if let (Some((best_bid, bid_quantity)), Some((best_ask, ask_quantity))) = (best_bid_info, best_ask_info) {
+            // Currently calculating in u64
                 let mid_price = ((best_bid * bid_quantity) as f64 + (best_ask * ask_quantity) as f64) /
                     (bid_quantity + ask_quantity) as f64;
                 self.price_tx.send(mid_price).expect("Could not send pricing over to feed sim");
@@ -157,60 +167,59 @@ impl App {
     }
 
     fn draw(&self, frame: &mut ratatui::Frame) {
-            // Split view into chunks
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .constraints([Constraint::Length(3), Constraint::Percentage(70), Constraint::Percentage(30)])
-                .split(frame.area());
+        // Split view into chunks
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Length(3), Constraint::Percentage(70), Constraint::Percentage(30)])
+            .split(frame.area());
 
-            // Get bid and ask BTree
-            let bids: std::collections::BTreeMap<u64, Vec<Order>> = self.book.get_bids();
-            let asks: std::collections::BTreeMap<u64, Vec<Order>> = self.book.get_asks();
+        // Get bid and ask BTree
+        let bids: std::collections::BTreeMap<u64, Vec<Order>> = self.book.get_bids();
+        let asks: std::collections::BTreeMap<u64, Vec<Order>> = self.book.get_asks();
 
-            let (best_bid, best_ask) = {
-                let best_bid = bids.last_key_value().map(|(price, _)| *price);
-                let best_ask = asks.first_key_value().map(|(price, _)| *price);
-                (best_bid, best_ask)
-            };
+        let (best_bid, best_ask) = {
+            let best_bid = bids.last_key_value().map(|(price, _)| *price);
+            let best_ask = asks.first_key_value().map(|(price, _)| *price);
+            (best_bid, best_ask)
+        };
 
-            let spread_text = match (best_bid, best_ask) {
-                (Some(b), Some(a)) => format!(
-                    "Bid: {}  Ask: {}  Spread: {}  Trades: {}  Vol: {}  VWAP: {:.2}",
-                    b, a, a.saturating_sub(b), self.statistics.trade_count, self.statistics.total_volume,
-                    self.statistics.vwap().unwrap_or(0.0)
-                ),
-                _ => "Waiting for liquidity...".to_string(),
-            };
+        let spread_text = match (best_bid, best_ask) {
+            (Some(b), Some(a)) => format!(
+                "Bid: {}  Ask: {}  Spread: {}  Trades: {}  Vol: {}  VWAP: {:.2}",
+                b, a, a.saturating_sub(b), self.statistics.trade_count, self.statistics.total_volume,
+                self.statistics.vwap().unwrap_or(0.0)
+            ),
+            _ => "Waiting for liquidity...".to_string(),
+        };
 
-            // Top view
-            let stats_widget = Paragraph::new(spread_text)
+        // Top view
+        let stats_widget = Paragraph::new(spread_text)
             .block(Block::default().borders(Borders::ALL).title("Market Stats"));
-            frame.render_widget(stats_widget, chunks[0]);
+        frame.render_widget(stats_widget, chunks[0]);
 
-            // Mid-view
-            let mid_chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
-                .split(chunks[1]);
+        // Mid-view
+        let mid_chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+            .split(chunks[1]);
 
-            let visible_candles = &self.statistics.candles;
-            let min_price = visible_candles.iter().map(|o| o.low).min().unwrap_or(0) as f64;
-            let max_price = visible_candles.iter().map(|o| o.high).max().unwrap_or(0) as f64;
-            let x_max = 3.0 * 30.0 + 1.0;
+        let visible_candles = &self.statistics.candles;
+        let min_price = visible_candles.iter().map(|o| o.low).min().unwrap_or(0) as f64;
+        let max_price = visible_candles.iter().map(|o| o.high).max().unwrap_or(0) as f64;
+        let x_max = 3.0 * 30.0 + 1.0;
 
-            let canvas_widget = Canvas::default()
+        let canvas_widget = Canvas::default()
             .block(Block::bordered().title("Candle Chart"))
             .x_bounds([0.0, x_max])
             .y_bounds([min_price, max_price])
             .paint(|ctx| {
-
                 let next_index = self.statistics.candles.len();
                 for (index, candle) in self.statistics.candles.iter().enumerate().rev().take(30) {
-                    let color = if candle.close >= candle.open { Color::Green} else { Color::Red };
+                    let color = if candle.close >= candle.open { Color::Green } else { Color::Red };
 
                     ctx.draw(&Line {
                         x1: index as f64 * 3.0 + 1.0,
-                        y1: candle.low as f64 ,
+                        y1: candle.low as f64,
                         x2: index as f64 * 3.0 + 1.0,
                         y2: candle.high as f64,
                         color,
@@ -225,10 +234,10 @@ impl App {
                 }
 
                 if let Some(current_candle) = &self.statistics.current_candle {
-                    let color = if current_candle.close >= current_candle.open { Color::Green} else { Color::Red };
+                    let color = if current_candle.close >= current_candle.open { Color::Green } else { Color::Red };
                     ctx.draw(&Line {
                         x1: next_index as f64 * 3.0 + 1.0,
-                        y1: current_candle.low as f64 ,
+                        y1: current_candle.low as f64,
                         x2: next_index as f64 * 3.0 + 1.0,
                         y2: current_candle.high as f64,
                         color,
@@ -244,71 +253,78 @@ impl App {
             });
 
 
-            frame.render_widget(canvas_widget, mid_chunks[0]);
+        frame.render_widget(canvas_widget, mid_chunks[0]);
 
-            let log_items: Vec<ListItem> = self.events.iter()
+        let log_items: Vec<ListItem> = self.events.iter()
             .rev()
             .take(30)
             .map(|e| ListItem::new(e.to_log_line()))
             .collect();
 
-            let log_list = List::new(log_items)
-                .block(Block::default().borders(Borders::ALL).title("Trade Log"));
-            frame.render_widget(log_list, mid_chunks[1]);
+        let log_list = List::new(log_items)
+            .block(Block::default().borders(Borders::ALL).title("Trade Log"));
+        frame.render_widget(log_list, mid_chunks[1]);
 
-            // Bottom view (bids & asks)
-            let bottom_chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
-                .split(chunks[2]);
-            
-            let max_bid_quantity = bids.values()
+        // Bottom view (bids & asks)
+        let bottom_chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+            .split(chunks[2]);
+
+        let max_bid_quantity = App::get_max_quantity(&bids);
+
+        let max_ask_quantity = App::get_max_quantity(&asks);
+
+        // Descending
+        let mut bid_levels: Vec<(u64, u64)> = App::sort_book_level(&bids, false);
+
+        // Ascending
+        let mut ask_levels: Vec<(u64, u64)> = App::sort_book_level(&asks, true);
+
+        let bar_width = 50;
+
+        // Bid quantity display
+        let bid_lines: Vec<String> = App::create_lines(bid_levels, max_bid_quantity, bar_width);
+
+        let buy_widget = List::new(bid_lines)
+            .block(Block::default().borders(Borders::ALL).title("Bids"));
+        frame.render_widget(buy_widget, bottom_chunks[0]);
+
+        // Ask quantity display
+        let ask_lines: Vec<String> = App::create_lines(ask_levels, max_ask_quantity, bar_width);
+
+        let ask_widget = List::new(ask_lines)
+            .block(Block::default().borders(Borders::ALL).title("Asks"));
+        frame.render_widget(ask_widget, bottom_chunks[1]);
+    }
+
+    fn sort_book_level(book: &BTreeMap<u64, Vec<Order>>, ascending: bool) -> Vec<(u64, u64)> {
+        // Sort by price, returns (price, quantity)
+        let mut levels: Vec<(u64, u64)> = book.iter()
+            .map(|(price, orders)| (*price, orders.iter().map(|o| o.quantity).sum::<u64>()))
+            .collect();
+        if ascending {
+            levels.sort_by(|a,b| a.0.cmp(&b.0))
+        } else {
+            levels.sort_by(|a,b| b.0.cmp(&a.0))
+        }
+        levels
+    }
+
+    fn get_max_quantity(book: &BTreeMap<u64, Vec<Order>>) -> u64 {
+        book.values()
             .map(|orders| orders.iter().map(|o| o.quantity).sum::<u64>())
             .max()
-            .unwrap_or(0);
-        
-            let max_ask_quantity = asks.values()
-                .map(|orders| orders.iter().map(|o| o.quantity).sum::<u64>())
-                .max()
-                .unwrap_or(0);
-            
-            let mut bid_levels: Vec<(u64, u64)> = bids.iter()
-            .map(|(price, orders)| (*price, orders.iter().map(|o| o.quantity).sum::<u64>()))
-            .collect();
-            bid_levels.sort_by(|a, b| b.0.cmp(&a.0)); // Descending
-        
-            let mut ask_levels: Vec<(u64, u64)> = asks.iter()
-            .map(|(price, orders)| (*price, orders.iter().map(|o| o.quantity).sum::<u64>()))
-            .collect();
-            ask_levels.sort_by(|a, b| a.0.cmp(&b.0)); // Ascending
-            
-            let bar_width = 50;
+            .unwrap_or(0)
+    }
 
-            // Bid
-            let bid_lines: Vec<String> = bid_levels.iter()
+    fn create_lines(levels: Vec<(u64, u64)>, max_quantity: u64, bar_width: i32) -> Vec<String> {
+        levels.iter()
             .map(|(price, quantity)| {
-                let filled = ((*quantity as f64 / max_bid_quantity as f64) * bar_width as f64).round() as usize;
+                let filled = ((*quantity as f64 / max_quantity as f64) * bar_width as f64).round() as usize;
                 let bar = "█".repeat(filled);
                 format!("{:>5} orders @ {:>5} {}", quantity, price, bar)
             })
-            .collect();
-        
-        
-            let buy_widget = List::new(bid_lines)
-                .block(Block::default().borders(Borders::ALL).title("Bids"));
-            frame.render_widget(buy_widget, bottom_chunks[0]);
-
-            // Ask
-            let ask_lines: Vec<String> = ask_levels.iter()
-            .map(|(price, quantity)| {
-                let filled = ((*quantity as f64 / max_ask_quantity as f64) * bar_width as f64).round() as usize;
-                let bar = "█".repeat(filled);
-                format!("{:>5} orders @ {:>5} {}", quantity, price, bar)
-            })
-            .collect();
-
-            let ask_widget = List::new(ask_lines)
-                .block(Block::default().borders(Borders::ALL).title("Asks"));
-            frame.render_widget(ask_widget, bottom_chunks[1]);
+            .collect()
     }
 }
